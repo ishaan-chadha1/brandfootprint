16ae090f3a740baa15c362073800858d6c7c76cc
ishaan-chadha1 <ishaan.chadha12@gmail.com>
2023-04-05 12:39:38 +0530

    Changes to Header.

--- src/components/header.js
+++ src/components/header.js
@@ -2,34 +2,8 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-900 text-white shadow-md">
-      <nav className="container mx-auto flex justify-between items-center py-4">
-        <a href="/" className="text-3xl font-bold">
-          BrandFootprint
-        </a>
-        <ul className="flex flex-col gap-4 list-none pl-0">
-          <li>
-            <a href="/" className="hover:text-gray-400 block">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="hover:text-gray-400 block">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/services" className="hover:text-gray-400 block">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="/contact" className="hover:text-gray-400 block">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <header className="bg-gradient p-4 text-white">
+      <h1 className="text-4xl font-bold">Your Company</h1>
     </header>
   );
 };
