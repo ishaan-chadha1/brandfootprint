e21f1fd826cce38e724d385c5e793f0eee1cff98
ishaan-chadha1 <ishaan.chadha12@gmail.com>
2023-04-05 16:34:17 +0530

    New update to landing page

--- src/components/content.js
+++ src/components/content.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Content = ({ title, text }) => {
+const Content = ({ title, date, description }) => {
   return (
-    <section>
-      <h2>{title}</h2>
-      <p>{text}</p>
-    </section>
+    <div className="bg-white shadow-lg rounded-md p-4 m-4">
+      <h2 className="text-2xl font-bold mb-2">{title}</h2>
+      <p className="text-gray-600 mb-2">{date}</p>
+      <p className="text-gray-800">{description}</p>
+    </div>
   );
 };
 
